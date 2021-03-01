62b02399552a5dda441e8d05ecb46acba3f4d9b2
Yael Cantero <78514536+JYaelCE@users.noreply.github.com>
2021-02-28 22:49:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php include_once 'https://jyaelce.github.io/proyecto/css/estilo.css';?>
+<?php include_once 'https://jyaelce.github.io/proyecto/index.html';?>
  </body>
 </html>
