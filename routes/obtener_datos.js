6fb23b0f26b6619ef4d5ac55907f1d7b76b5f696
Yael Cantero <78514536+JYaelCE@users.noreply.github.com>
2021-03-04 20:29:19 -0600

    Add files via upload

--- routes/obtener_datos.js
+++ routes/obtener_datos.js
@@ -9,12 +9,13 @@ obtener_datos.get("/obtener_datos_usuario/:correo", (req, res) => {
     const solicitud_usuario = "select * from usuario u where u.correo = ?"
     conexion.query(solicitud_usuario, [dato], (err, rows, fields) =>{
         if(err){
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })    
 })
 
@@ -24,12 +25,13 @@ obtener_datos.get("/obtener_datos_experiencia/:correo", (req, res) => {
     const solicitud_experiencia = "select nombre_empresa, puesto_e, ano_inicio_e, ano_final_e, descripcion_e from experiencia e where e.correo = ?"
     conexion.query(solicitud_experiencia, [dato], (err, rows, fields) => {
         if (err) {
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })
 })
 
@@ -39,12 +41,13 @@ obtener_datos.get("/obtener_datos_educacion/:correo", (req, res) => {
     const solicitud_experiencia = "select nombre_escuela, carrera, ano_inicio_c, ano_final_c, descripcion_c from educacion e where e.correo = ?"
     conexion.query(solicitud_experiencia, [dato], (err, rows, fields) => {
         if (err) {
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })
 })
 
@@ -54,12 +57,13 @@ obtener_datos.get("/obtener_datos_software/:correo", (req, res) => {
     const solicitud_experiencia = "select software, nivel_s from software s where s.correo = ?"
     conexion.query(solicitud_experiencia, [dato], (err, rows, fields) => {
         if (err) {
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })
 })
 
@@ -69,12 +73,13 @@ obtener_datos.get("/obtener_datos_skills/:correo", (req, res) => {
     const solicitud_experiencia = "select skill, nivel_h from skills s where s.correo = ?"
     conexion.query(solicitud_experiencia, [dato], (err, rows, fields) => {
         if (err) {
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })
 })
 
@@ -84,12 +89,13 @@ obtener_datos.get("/obtener_datos_redes/:correo", (req, res) => {
     const solicitud_experiencia = "select link, tipo from redes r where r.correo = ?"
     conexion.query(solicitud_experiencia, [dato], (err, rows, fields) => {
         if (err) {
+            conexion.end()
             console.log("hubo un error: " + err)
             res.redirect('/error')
             return
         }
-        res.json(rows)
         conexion.end()
+        res.json(rows)
     })
 })
 
